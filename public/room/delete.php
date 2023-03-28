1198297ae09d49ae1b8fa876faf493e6f5085d1c
Vilem <vilem.cerman@seznam.cz>
2023-03-28 13:09:03 +0200

    clean the code

--- public/room/delete.php
+++ public/room/delete.php
@@ -3,7 +3,6 @@ require_once __DIR__ . "/../../bootstrap/bootstrap.php";
 
 class RoomDeletePage extends CRUDPage
 {
-
     protected function prepare(): void
     {
         parent::prepare();
@@ -12,18 +11,18 @@ class RoomDeletePage extends CRUDPage
         if (!$roomId)
             throw new BadRequestException();
 
+
+
         //když poslal data
         $success = Room::deleteByID($roomId);
 
         //přesměruj
         $this->redirect(self::ACTION_DELETE, $success);
     }
-
     protected function pageBody()
     {
         return "";
     }
-
 }
 
 $page = new RoomDeletePage();
