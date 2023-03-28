ff19ace9e89bed604a14d02acd69bc73b88f4ecc
Vilem <vilem.cerman@seznam.cz>
2023-03-28 13:08:42 +0200

    add passChange page

--- public/passChange.php
+++ public/passChange.php
@@ -1,7 +1,15 @@
 <?php //TODO password change page
 require_once __DIR__ . "/../bootstrap/bootstrap.php";
 
-class passChange extends LoginPage
+class passChange extends BasePage
 {
-
+    protected function pageBody() :string
+    {
+        return "<a href='index.php'>Zpět na hlavní stránku</a>
+                <form method='post'>
+                    Jméno: <input type='text' name='login' value='$this->login'/><br />
+                    Heslo: <input type='password' name='password'/><br />
+                    <input type='submit' value='Submit' />
+                </form>";
+    }
 }
