dd493536be7817befb8f006cd916cdd69ef4acb4
Vilem <vilem.cerman@seznam.cz>
2023-03-30 10:27:27 +0200

    add keys to employee update form

--- public/staff/update.php
+++ public/staff/update.php
@@ -6,6 +6,9 @@ class EmployeeUpdatePage extends CRUDPage
     private ?Employee $employee;
     private ?array $errors = [];
     private int $state;
+    private $keys;
+    private $allKeys;
+    private $rooms;
 
     protected function prepare(): void
     {
@@ -25,12 +28,36 @@ class EmployeeUpdatePage extends CRUDPage
             if (!$this->employee)
                 throw new NotFoundException();
 
+            $stmt = PDOProvider::get()->prepare("SELECT `key`.key_id AS key_id, `key`.room AS room, `room`.name AS room_name FROM `key` INNER JOIN `room` ON `key`.employee = :employeeId AND `key`.room = `room`.room_id ORDER BY room ASC");
+            $stmt->execute(['employeeId' => $employeeId]);
+            //$this->keys = $stmt->fetchAll();
+
+            $stmtRoom = PDOProvider::get()->prepare("SELECT room_id, no, name FROM room ORDER BY room_id ASC");
+            $stmtRoom->execute([]);
+
+            $keysAvailable = $key = $stmt->fetch();
+            while ($room = $stmtRoom->fetch())
+            {
+                $active = false;
+                if($keysAvailable && $key->room == $room->room_id){
+                    $active = true;
+                    $keysAvailable = $key = $stmt->fetch();
+                }
+                $this->rooms[] = [
+                    'room_id' => $room->room_id,
+                    'no' => $room->no,
+                    'name' => $room->name,
+                    'isActive' => $active
+                ];
+            }
         }
 
         //když poslal data
         elseif($this->state === self::STATE_DATA_SENT) {
             //načti je
             $this->employee = Employee::readPost();
+            $this->keys = filter_input(INPUT_POST, 'keys',FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+            //dump($this->keys);
 
             //zkontroluj je, jinak formulář
             $this->errors = [];
@@ -41,11 +68,36 @@ class EmployeeUpdatePage extends CRUDPage
             }
             else
             {
+                //dump($_POST);
+                $employeeId = $this->employee->employee_id;
                 //ulož je
-               $success = $this->employee->update();
+
+                $stmtUserKeys = PDOProvider::get()->prepare("SELECT room, key_id FROM `key` WHERE `employee` = :employeeId ORDER BY room ASC");
+                $stmtUserKeys->execute(['employeeId' => $this->employee->employee_id]);
+                //$userKeys = $stmtUserKeys->fetchAll();
+                while($key = $stmtUserKeys ->fetch()){
+                    $userKeys[$key->room] = $key->key_id;
+                }
+
+                $stmt = PDOProvider::get()->prepare("SELECT * FROM `key` INNER JOIN `room` ON `key`.employee = :employeeId AND `key`.room = `room`.room_id ORDER BY room ASC");
+                //$stmt->execute(['employeeId' => $employeeId]);
+
+                $allRooms = Room::getAll();
+
+                foreach ($allRooms AS $room){
+                    if(isset($userKeys[$room->room_id]) && !isset($this->keys[$room->room_id])){
+                        Key::deleteByID($userKeys[$room->room_id]);
+                    }
+                    else if(!isset($userKeys[$room->room_id]) && isset($this->keys[$room->room_id])){
+                        $key = new Key(null, $room->room_id, $this->employee->employee_id);
+                        $key->insert();
+                    }
+                }
+
+                $success = $this->employee->update();
 
                 //přesměruj
-               $this->redirect(self::ACTION_UPDATE, $success);
+                $this->redirect(self::ACTION_UPDATE, $success);
             }
         }
     }
@@ -57,6 +109,7 @@ class EmployeeUpdatePage extends CRUDPage
             [
                 'employee' => $this->employee,
                 'errors' => $this->errors,
+                'rooms' => $this->rooms,
                 'header' => $this->title.' '.$this->employee->name.' '.$this->employee->surname
             ]
         );
