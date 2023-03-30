4a3405fed76062737b22fcd19f92018ca1aad897
Vilem <vilem.cerman@seznam.cz>
2023-03-30 17:37:51 +0200

    fix logout

--- public/logout.php
+++ public/logout.php
@@ -3,11 +3,24 @@ require_once __DIR__ . "/../bootstrap/bootstrap.php";
 
 class LogoutPage extends BasePage
 {
-    protected ?string $login = null;
-    protected ?string $pass = null;
+    private ?string $header = "Odhlášení proběhlo úspěšně";
+
+    protected function prepare(): void
+    {
+        parent::prepare();
+        //pokud přišel výsledek, zachytím ho
+        $crudResult = filter_input(INPUT_GET, 'success', FILTER_VALIDATE_INT);
+        $crudAction = filter_input(INPUT_GET, 'action');
+
+        if (is_int($crudResult)) {
+            if($crudResult == 1 && $crudAction == "updatePass"){
+                $this->header = "Změna hesla proběhla úspěšně";
+            }
+        }
+    }
+
     public function __construct()
     {
-
         $this->title = "Odhlášení";
     }
 
@@ -18,14 +31,12 @@ class LogoutPage extends BasePage
 
     protected function pageHeader(): string
     {
-        //session_start();
         unset($_SESSION['user']);
         unset($_SESSION['admin']);
         unset($_SESSION['id']);
-        return "<h1>Odhlášení proběhlo úspěšně</h1>
+        return "<h1>".$this->header."</h1>
                 <a href='index.php'>Zpět na hlavní stránku</a>";
     }
-
 }
 
 $page = new LogoutPage();
